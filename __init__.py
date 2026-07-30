f9fc75755382e9e5924883c8dcaacbf180764467
archronusgaming-code <archronusgaming@gmail.com>
2026-07-30 15:55:04 -0400

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,9 @@
+from .calibration_node import ScreenCalibrationNode
+
+NODE_CLASS_MAPPINGS = {
+    "ScreenRemovalCalibration": ScreenCalibrationNode,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "ScreenCalibration": "Screen Calibration",
+}
